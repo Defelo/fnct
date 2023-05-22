ed007908e0e62f679873b6f57e5baf0f6f9d3c30
Defelo <elodef42@gmail.com>
2023-05-22 14:56:49 +0200

    Improve documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,21 @@
 //! Simple caching library that supports cache invalidation via tags.
 //!
 //! #### Example
-//! ```no_run
+//! ```
 //! use std::time::Duration;
 //!
-//! use fnct::{backend::AsyncRedisBackend, format::PostcardFormatter, key, AsyncCache};
+//! use fnct::{backend::AsyncRedisBackend, format::PostcardFormatter, keyfn, AsyncCache};
 //! use redis::{aio::MultiplexedConnection, Client};
 //!
 //! struct Application {
 //!     cache: AsyncCache<AsyncRedisBackend<MultiplexedConnection>, PostcardFormatter>,
 //! }
 //!
+//! keyfn!(my_cache_key(a: i32, b: i32));
 //! impl Application {
-//!     async fn cached_function(&self, a: i32, b: i32) -> i32 {
+//!     async fn test(&self, a: i32, b: i32) -> i32 {
 //!         self.cache
-//!             .cached(key!(a, b), &["sum"], None, async move {
+//!             .cached(my_cache_key(a, b), &["sum"], None, async {
 //!                 // expensive computation
 //!                 a + b
 //!             })
@@ -23,21 +24,24 @@
 //!     }
 //! }
 //!
-//! # async {
-//! let client = Client::open("redis://localhost:6379/0").unwrap();
-//! let conn = client.get_multiplexed_async_connection().await.unwrap();
-//! let app = Application {
-//!     cache: AsyncCache::new(
-//!         AsyncRedisBackend::new(conn, "my_application".to_owned()),
-//!         PostcardFormatter,
-//!         Duration::from_secs(600),
-//!     ),
-//! };
-//! assert_eq!(app.cached_function(1, 2).await, 3); // run expensive computation and fill cache
-//! assert_eq!(app.cached_function(1, 2).await, 3); // load result from cache
-//! app.cache.pop_key((1, 2)).await.unwrap(); // invalidate cache by key
-//! app.cache.pop_tag("sum").await.unwrap(); // invalidate cache by tag
-//! # };
+//! #[tokio::main]
+//! async fn main() {
+//! # if let Ok(REDIS_SERVER) = std::env::var("REDIS_SERVER") {
+//!     let client = Client::open(REDIS_SERVER).unwrap();
+//!     let conn = client.get_multiplexed_async_connection().await.unwrap();
+//!     let app = Application {
+//!         cache: AsyncCache::new(
+//!             AsyncRedisBackend::new(conn, "my_application".to_owned()),
+//!             PostcardFormatter,
+//!             Duration::from_secs(600),
+//!         ),
+//!     };
+//!     assert_eq!(app.test(1, 2).await, 3); // run expensive computation and fill cache
+//!     assert_eq!(app.test(1, 2).await, 3); // load result from cache
+//!     app.cache.pop_key(my_cache_key(1, 2)).await.unwrap(); // invalidate cache by key
+//!     app.cache.pop_tag("sum").await.unwrap(); // invalidate cache by tag
+//! # }
+//! }
 //! ```
 
 #![forbid(unsafe_code)]
@@ -61,6 +65,45 @@ pub mod backend;
 pub mod format;
 
 /// A cache that can be used asynchronously.
+///
+/// #### Example
+/// ```
+/// # use fnct::{AsyncCache, backend::AsyncRedisBackend, format::PostcardFormatter};
+/// # use redis::{Client, aio::MultiplexedConnection};
+/// # use std::time::Duration;
+/// # #[tokio::main]
+/// # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// # if let Ok(REDIS_SERVER) = std::env::var("REDIS_SERVER") {
+/// # let client = Client::open(REDIS_SERVER)?;
+/// # let conn = client.get_multiplexed_async_connection().await?;
+/// # let backend = AsyncRedisBackend::new(conn, "test".to_owned());
+/// # let formatter = PostcardFormatter;
+/// let cache = AsyncCache::new(backend, formatter, Duration::from_secs(10));
+///
+/// assert!(cache.get::<i32, _>("foo").await?.is_none()); // cache is empty initially
+/// cache
+///     .put("foo", 42, &[], Some(Duration::from_secs(1)))
+///     .await?; // store a value in the cache
+/// assert_eq!(cache.get::<i32, _>("foo").await?.unwrap(), 42); // retrieve the value
+/// tokio::time::sleep(Duration::from_secs(1)).await; // wait 1 second
+/// assert!(cache.get::<i32, _>("foo").await?.is_none()); // the value expired
+///
+/// cache.put("foo", 1337, &["my_tag"], None).await?; // store value with a tag
+/// assert_eq!(cache.get::<i32, _>("foo").await?.unwrap(), 1337); // retrieve the value
+/// cache.pop_tag("my_tag").await?; // delete value by tag
+/// assert!(cache.get::<i32, _>("foo").await?.is_none());
+///
+/// cache.put("foo", 42, &["1", "2", "3"], None).await?; // store values with different tags
+/// cache.put("bar", 1337, &["1", "2", "4"], None).await?;
+/// assert_eq!(cache.get::<i32, _>("foo").await?.unwrap(), 42);
+/// assert_eq!(cache.get::<i32, _>("bar").await?.unwrap(), 1337);
+/// cache.pop_tags(&["2", "3"]).await?; // delete values tagged with both "2" and "3"
+/// assert!(cache.get::<i32, _>("foo").await?.is_none()); // deleted (was tagged with "2" and "3")
+/// assert_eq!(cache.get::<i32, _>("bar").await?.unwrap(), 1337); // kept (not tagged with "3")
+/// # }
+/// # Ok(())
+/// # }
+/// ```
 #[derive(Debug, Clone)]
 pub struct AsyncCache<B, S> {
     backend: B,
@@ -74,6 +117,18 @@ where
     S: Formatter,
 {
     /// Create a new [`AsyncCache`].
+    ///
+    /// #### Example
+    /// ```no_run
+    /// # use fnct::{AsyncCache, backend::AsyncRedisBackend, format::PostcardFormatter};
+    /// # use std::time::Duration;
+    /// # let conn: redis::aio::MultiplexedConnection = todo!();
+    /// let cache = AsyncCache::new(
+    ///     AsyncRedisBackend::new(conn, "my_namespace".into()),
+    ///     PostcardFormatter,
+    ///     Duration::from_secs(600),
+    /// );
+    /// ```
     pub fn new(backend: B, formatter: S, default_ttl: Duration) -> Self {
         Self {
             backend,
@@ -83,6 +138,15 @@ where
     }
 
     /// Create a new cache that uses a different formatter.
+    ///
+    /// #### Example
+    /// ```no_run
+    /// # use fnct::{backend::AsyncRedisBackend, AsyncCache, format::{PostcardFormatter, JsonFormatter}};
+    /// # use redis::aio::MultiplexedConnection;
+    /// type MyCache<F> = AsyncCache<AsyncRedisBackend<MultiplexedConnection>, F>;
+    /// let postcard_cache: MyCache<PostcardFormatter> = todo!();
+    /// let json_cache: MyCache<JsonFormatter> = postcard_cache.with_formatter(JsonFormatter);
+    /// ```
     pub fn with_formatter<T: Formatter>(&self, formatter: T) -> AsyncCache<B, T>
     where
         B: Clone,
@@ -95,6 +159,22 @@ where
     }
 
     /// Wrap a function to add a caching layer.
+    ///
+    /// #### Example
+    /// ```no_run
+    /// # use fnct::{backend::AsyncRedisBackend, format::PostcardFormatter, AsyncCache};
+    /// # use redis::aio::MultiplexedConnection;
+    /// let cache: AsyncCache<AsyncRedisBackend<MultiplexedConnection>, PostcardFormatter> = todo!();
+    /// # async {
+    /// let result = cache
+    ///     .cached("my_cache_key", &["tag1", "tag2"], None, async {
+    ///         (1..=1000000).sum::<u64>()
+    ///     })
+    ///     .await
+    ///     .unwrap();
+    /// assert_eq!(result, 500000500000);
+    /// # };
+    /// ```
     pub async fn cached<T, K, F>(
         &self,
         key: K,
@@ -117,6 +197,26 @@ where
 
     /// Wrap a function to add a caching layer.
     /// Cache [`Option::Some`] variants only.
+    ///
+    /// #### Example
+    /// ```no_run
+    /// # use fnct::{backend::AsyncRedisBackend, format::PostcardFormatter, AsyncCache};
+    /// # use redis::aio::MultiplexedConnection;
+    /// let cache: AsyncCache<AsyncRedisBackend<MultiplexedConnection>, PostcardFormatter> = todo!();
+    /// # async {
+    /// let result = cache
+    ///     .cached_option("my_cache_key", &["tag1", "tag2"], None, async {
+    ///         if todo!() {
+    ///             Some((1..=1000000).sum::<u64>()) // cached
+    ///         } else {
+    ///             None // not cached
+    ///         }
+    ///     })
+    ///     .await
+    ///     .unwrap();
+    /// assert_eq!(result, Some(500000500000));
+    /// # };
+    /// ```
     pub async fn cached_option<T, K, F>(
         &self,
         key: K,
@@ -136,6 +236,26 @@ where
 
     /// Wrap a function to add a caching layer.
     /// Cache [`Result::Ok`] variants only.
+    ///
+    /// #### Example
+    /// ```no_run
+    /// # use fnct::{backend::AsyncRedisBackend, format::PostcardFormatter, AsyncCache};
+    /// # use redis::aio::MultiplexedConnection;
+    /// let cache: AsyncCache<AsyncRedisBackend<MultiplexedConnection>, PostcardFormatter> = todo!();
+    /// # async {
+    /// let result = cache
+    ///     .cached_result("my_cache_key", &["tag1", "tag2"], None, async {
+    ///         if todo!() {
+    ///             Ok((1..=1000000).sum::<u64>()) // cached
+    ///         } else {
+    ///             Err("test") // not cached
+    ///         }
+    ///     })
+    ///     .await
+    ///     .unwrap();
+    /// assert_eq!(result, Ok(500000500000));
+    /// # };
+    /// ```
     pub async fn cached_result<T, E, K, F>(
         &self,
         key: K,
@@ -175,6 +295,10 @@ where
     }
 
     /// Insert a new or update an existing cache entry.
+    ///
+    /// The value is automatically invalidated after the given `ttl` has elapsed. It can also be
+    /// invalidated manually by using one of the [`pop_key`](Self::pop_key),
+    /// [`pop_tag`](Self::pop_tag) or [`pop_tags`](Self::pop_tags) functions.
     pub async fn put<T, K>(
         &self,
         key: K,
@@ -219,6 +343,8 @@ where
     }
 
     /// Invalidate all cache entries that are associated with ALL of the given tags.
+    ///
+    /// If `tags` is empty, all cached values (in the given namespace) are invalidated.
     pub async fn pop_tags(&self, tags: &[&str]) -> Result<(), Error<B, S>> {
         self.backend
             .pop_tags(tags)
